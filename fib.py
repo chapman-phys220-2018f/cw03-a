eb35a225ed9bdd8b849ddd3f8b723fdf24e7c27f
wheatonabigail <wheaton@chapman.edu>
2018-09-13 17:50:14 +0000

    Executable Python Script

---
+++ fib.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sequences
+
+def main(argv):
+    print( sequences.fibonacci(int(argv[1]))[int(int(argv[1])-1)] )
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
