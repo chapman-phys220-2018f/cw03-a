5ef66dabba38d3b4e8ee14296581a470e7b89ba1
wheatonabigail <wheaton@chapman.edu>
2018-09-19 21:57:47 +0000

    finished

--- fib.py
+++ fib.py
@@ -9,7 +9,7 @@
 import sequences
 
 def main(argv):
-    print( sequences.fibonacci(int(argv[1]))[int(int(argv[1])-1)], end=",")
+    print( sequences.fibonacci(int(argv[1]))[int(int(argv[1])-1)])
 
 if __name__ == "__main__":
     import sys
